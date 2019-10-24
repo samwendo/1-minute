755a5a994dcab68b5725accd36b6bd8b40855d01
samwendo <wendosam21@gmail.com>
2019-10-24 09:30:22 +0300

    moved email function to class config

--- config.py
+++ config.py
@@ -5,14 +5,16 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY=os.environ.get('SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sam:54321@localhost/ti'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI= os.environ.get("DATABASE_URL")
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
 class ProdConfig(Config):
@@ -22,7 +24,8 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI= os.environ.get("DATABASE_URL")
+
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sam:54321@localhost/ti'
 
 
 class TestConfig(Config):
@@ -35,7 +38,8 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sam:54321@localhost/ti'
+
+    SQLALCHEMY_DATABASE_URI= os.environ.get("DATABASE_URL")
 
     DEBUG = True
 
