c48d11eb67573c992a81ec4b32a4a91e50224c27
samwendo <wendosam21@gmail.com>
2019-10-24 10:25:08 +0300

    debug error

--- config.py
+++ config.py
@@ -6,15 +6,15 @@ class Config:
     '''
     SECRET_KEY=os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sam:54321@localhost/ti'
     SQLALCHEMY_DATABASE_URI= os.environ.get("DATABASE_URL")
-    UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
 class ProdConfig(Config):
@@ -25,11 +25,12 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
 
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sam:54321@localhost/ti'
-
 
 class TestConfig(Config):
+    
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sam:54321@localhost/ti_test'
+
+    DEBUG = True
 
 class DevConfig(Config):
     '''
@@ -38,10 +39,11 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sam:54321@localhost/ti'
+    
     SQLALCHEMY_DATABASE_URI= os.environ.get("DATABASE_URL")
 
-    DEBUG = True
+    
 
 
 config_options = {
